836da4625fa10ebeb0092766c445653f1b9a0400
calascionec <student@epicodus.com>
2015-10-28 16:32:38 -0700

    start work on custom theme

---
+++ sites/all/themes/pro_theme/page.tpl.php
@@ -0,0 +1,41 @@
+<?php ?>
+
+
+<div id="root">
+  <div class="header-wrap">
+    <div class="header">
+
+    </div>
+  </div>
+
+  <div class="nav-wrap">
+    <div class="nav">
+
+    </div>
+  </div>
+
+  <div class="slideshow-wrap">
+    <div class="slideshow">
+
+    </div>
+  </div>
+
+  <div class="body-wrap">
+    <div class="body">
+      <div class="page-content"><?php print render($page['content']); ?></div>
+      <div class="page-right"><?php print render($page['sidebar_right']); ?></div>
+    </div>
+  </div>
+
+  <div class="sub-footer-wrap">
+    <div class="sub-footer">
+
+    </div>
+  </div>
+
+  <div class="footer-wrap">
+    <div class="footer">
+
+    </div>
+  </div>
+</div>
