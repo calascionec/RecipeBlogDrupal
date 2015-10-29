f76fcf2e6df714a2d1ac1f3cecea8785a5fa4593
calascionec <chris.calascione@gmail.com>
2015-10-28 21:05:55 -0700

    add follow module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'database',
       'username' => 'admin',
       'password' => '131276',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
